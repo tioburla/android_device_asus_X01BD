f9ca6f8ead5263a96b9681d6f08262b9604d0066
tioburla <gabrielrochagbl@gmail.com>
2022-04-15 16:57:40 +0530

    Upload Files

---
+++ lineage_X01BD.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common PixelExperience stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# PixelExperience Properties
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_SUPPORTS_QUICK_TAP := true
+
+#Maintainer
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=TioBurla
+
+# Inherit from X01BD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_X01BD
+PRODUCT_DEVICE := X01BD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X01BDA
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ASUS_X01BD_1 \
+    PRODUCT_NAME=WW_X01BD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 73 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := "asus/WW_X01BD/ASUS_X01BD_1:10/QKQ1/17.2018.2012.434-20201203:user/release-keys"
